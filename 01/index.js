ef9abe2783d31a4cb0329b47db8e231ba57e2287
= <=>
2022-11-03 19:20:03 +0300

    :tada: started series

---
+++ 01/index.js
@@ -0,0 +1,12 @@
+const os = require('os');
+const { add,multiply,subtract } = require('./math');
+
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+
+// console.log(__dirname)
+// console.log(__filename)
+console.log(add(10,20));
+console.log(multiply(2,5));
+console.log(subtract(20,10))
